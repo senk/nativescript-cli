54b07281ffe3590a6e1d741025d48746323e82ca
Peter Kanev <pip3r4o@gmail.com>
2017-08-03 14:14:05 +0300

    refactor method name

--- lib/services/android-project-service.ts
+++ lib/services/android-project-service.ts
@@ -347,7 +347,7 @@ export class AndroidProjectService extends projectServiceBaseLib.PlatformProject
 	}
 
 	public async preparePluginNativeCode(pluginData: IPluginData, projectData: IProjectData): Promise<void> {
-		// Add to platforms/android/dependencies.json
+		// Do nothing, the Android Gradle script will configure itself based on the input dependencies.json 
 	}
 
 	public async processConfigurationFilesFromAppResources(): Promise<void> {
@@ -357,7 +357,7 @@ export class AndroidProjectService extends projectServiceBaseLib.PlatformProject
 	public async removePluginNativeCode(pluginData: IPluginData, projectData: IProjectData): Promise<void> {
 		try {
 			// check whether the dependency that's being removed has native code
-			let pluginConfigDir = path.join(this.getPlatformData(projectData).projectRoot, "configurations", pluginData.name);
+			let pluginConfigDir = path.join(this.getPlatformData(projectData).projectRoot, "build", "configurations", pluginData.name);
 			if (this.$fs.exists(pluginConfigDir)) {
 				await this.cleanProject(this.getPlatformData(projectData).projectRoot, projectData);
 			}
@@ -389,6 +389,7 @@ export class AndroidProjectService extends projectServiceBaseLib.PlatformProject
 			.filter(AndroidProjectService.isNativeAndroidDependency)
 			.map(({ name, directory }) => ({ name, directory: path.relative(platformDir, directory) }));
 		const jsonContent = JSON.stringify(nativeDependencies, null, 4);
+
 		this.$fs.writeFile(dependenciesJsonPath, jsonContent);
 	}
 
